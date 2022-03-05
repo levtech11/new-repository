8b74d93e61803253f87f833cd49c3661029b45ea
Levan <levtech11@gmail.com>
2022-03-05 21:47:35 +0400

    3/5

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const myVar = process.env.HOST;
-console.log(myVar);
+// const prevBtns = document.querySelectorAll(".progress-step1")
+const form2 = document.getElementById("form2")
+
+
+const next1 = document.getElementById("progress-step7")
+
+
+next1.forEach(btn => {
+  btn.addEventListener{ "click", () =>{
+    updateFormSteps();
+  });
+  
+});
+
+
+function updateFormSteps() {
+    form2.classList.add("form-style-active");
+}
